231477dc478ec4cbe2ee0e7d5c4150de68380baa
Navone Ruben <Navone_ruben@msn.com>
2022-02-17 19:08:35 +0100

    Update style.js
    Ce morceau  de code ne fonctionnera pas il manque, le addEventListener et les selecteur.
    Je corrige l'autre code

--- asstes/js/style.js
+++ asstes/js/style.js
@@ -1,18 +1,25 @@
-var num = Math.floor(Math.random() * 1000) + 1;
+//Correction commentaire
+var num = Math.floor(Math.random() * 1000) + 1; //le +1 n'est pas nécéssaire, ici le math.random ira de 0 a 1000.99 grand max le floor arrondis a l'inférieur
 var NbEssais = 0;
+
 function Devine() {
-var choisi = document.form1.devine1.value;
-NbEssais++;
-status = "Nombre d'essais : " + NbEssais;
-if (choisi < num) 
-    document.form1.indice.value = "Non, le nombre est plus grand.";
-if (choisi > num)
-    document.form1.indice.value = "Non, le nombre est plus petit.";
-if (choisi == num) {
-    document.form1.indice.value = "Correct ! Vous avez trouvé en " + NbEssais + "essais.";
-    location.reload();
+    var choisi = document.form1.devine1.value;
+    NbEssais++;
+    
+    status = "Nombre d'essais : " + NbEssais; // tu n'a pas déclaré status 
+    
+    if (choisi < num) {
+        document.form1.indice.value = "Non, le nombre est plus grand.";
     }
-if (NbEssais == 10) {
-    document.form1.indice.value = ("Désolé, c'est fini. Le nombre correct était : " + num);
-    location.reload();
-}}
+    if (choisi > num){
+        document.form1.indice.value = "Non, le nombre est plus petit.";
+    }
+    if (choisi == num) {
+        document.form1.indice.value = "Correct ! Vous avez trouvé en " + NbEssais + "essais.";
+        location.reload();
+        }
+    if (NbEssais == 10) {
+        document.form1.indice.value = ("Désolé, c'est fini. Le nombre correct était : " + num);
+        location.reload();
+    }
+}
