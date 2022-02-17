8c37c8a7550d3a9dc548cee938fc1ae9932d07a0
Khouchi <95851166+Khouchi@users.noreply.github.com>
2022-02-17 14:46:42 +0100

    tp-jeudi

---
+++ asstes/js/style.js
@@ -0,0 +1,18 @@
+var num = Math.floor(Math.random() * 1000) + 1;
+var NbEssais = 0;
+function Devine() {
+var choisi = document.form1.devine1.value;
+NbEssais++;
+status = "Nombre d'essais : " + NbEssais;
+if (choisi < num) 
+    document.form1.indice.value = "Non, le nombre est plus grand.";
+if (choisi > num)
+    document.form1.indice.value = "Non, le nombre est plus petit.";
+if (choisi == num) {
+    window.alert("Correct ! Vous avez trouvé en " + NbEssais + "essais.");
+    location.reload();
+    }
+if (NbEssais == 10) {
+    window.alert("Désolé, c'est fini. Le nombre correct était : " + num);
+    location.reload();
+}}
